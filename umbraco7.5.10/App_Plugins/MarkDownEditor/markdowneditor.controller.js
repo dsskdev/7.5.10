d25fa62e611f52d96eca6c244112018b9d85047f
dssk dev <desskdev@gmail.com>
2017-03-06 16:42:49 +0000

    initial Codebase upload

---
+++ umbraco7.5.10/App_Plugins/MarkDownEditor/markdowneditor.controller.js
@@ -0,0 +1,21 @@
+﻿angular.module("umbraco")
+    .controller("My.MarkdownEditorController",
+    //inject umbracos assetsService
+    function ($scope, assetsService) {
+        //tell the assetsServices to load the markdown.editor libs from the markdown editors
+        //plugin folder
+        assetsService
+            .load([
+                "/App_Plugins/MarkDownEditor/lib/markdown.convertor.js",
+                "/App_Plugins/MarkdownEditor/lib/markdown.sanitizer.js",
+                "/App_Plugins/MarkdownsEditor/lib/markdown.editors.js"
+            ])
+            .then(function () {
+                //this function will execute when all dependencies have lodaed
+                var convertor2 = new Markdown. Convertor();
+                var editors2 = new Markdown.Editor(convertor2, "-" + $scope.model.alias);
+                editors2.run();
+            });
+        //load the seprate css for the editor to avoid it blocking our js loading
+        assetsService.loadCss("/App_Plugins/MarkDownEditor/lib/markdown.css");
+    });
